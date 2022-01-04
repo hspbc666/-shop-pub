6d6fcebee83f85c2aa7b447d633d3bf3bb12c682
jerry <309032663@qq.com>
2022-01-04 22:07:03 +0800

    update

---
+++ shop_android_kotlin/app/src/main/java/cn/hsp/shop/network/ApiService.kt
@@ -0,0 +1,50 @@
+/**
+ * 厦门大学计算机专业 | 前华为工程师
+ * 专注《零基础学编程系列》https://cxyxy.blog.csdn.net/article/details/121134634
+ * 包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
+ * 公众号：花生皮编程
+ */
+package cn.hsp.shop.network
+
+import cn.hsp.shop.network.request.AddNoteRequest
+import cn.hsp.shop.network.request.LoginRequest
+import cn.hsp.shop.network.request.ModifyNoteRequest
+import cn.hsp.shop.network.request.RegisterRequest
+import cn.hsp.shop.network.response.Note
+import cn.hsp.shop.network.response.LoginResp
+import cn.hsp.shop.network.response.RegisterResp
+import cn.hsp.shop.network.response.Result
+import retrofit2.http.*
+
+interface ApiService {
+
+    @POST("api/login")
+    @Headers("ignoreToken:true")
+    suspend fun login(@Body request: LoginRequest): Result<LoginResp?>?
+
+    @POST("api/register")
+    @Headers("ignoreToken:true")
+    suspend fun register(@Body request: RegisterRequest): Result<RegisterResp?>?
+
+    @GET("note/api/list/{userId}")
+    @Headers("ignoreToken:true")
+    suspend fun getDataList(@Path("userId") userId: Long): Result<List<Note>?>?
+
+    @GET("note/api/query/{noteId}")
+    suspend fun queryData(@Path("noteId") noteId: Long): Result<Note?>?
+
+    @GET("note/api/del/{noteId}")
+    suspend fun deleteData(@Path("noteId") noteId: Long): Result<String?>?
+
+    @POST("note/api/add")
+    suspend fun addData(@Body request: AddNoteRequest): Result<String?>?
+
+    @POST("note/api/modify")
+    suspend fun modifyData(@Body request: ModifyNoteRequest): Result<String?>?
+
+
+//    @GET("/article/list/{page}/json")
+//    suspend fun getArticleList(@Path("page") page: Int): ApiResult<Pagination<Article>>
+
+
+}
