416d468ea78c454a39d822dc669ffe0c228c744e
jerry <309032663@qq.com>
2022-01-08 11:27:14 +0800

    update

--- shop_android_kotlin/app/src/main/java/cn/hsp/shop/module/mine/MineFragment.kt
+++ shop_android_kotlin/app/src/main/java/cn/hsp/shop/module/mine/MineFragment.kt
@@ -1,7 +1,6 @@
 package cn.hsp.shop.module.mine
 
 import android.content.Intent
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -15,25 +14,51 @@ class MineFragment : BaseFragment() {
     private lateinit var userLogoIv: ImageView
     private lateinit var userNameTv: TextView
     private lateinit var userInfoLayout: View
+    private lateinit var allOrderTv: View
+    private lateinit var toPayTv: View
+    private lateinit var toDeliverTv: View
+    private lateinit var toReceiveTv: View
+    private lateinit var toCommentTv: View
+    private lateinit var afterSalesTv: View
     override fun layoutResId() = R.layout.fragment_mine
 
     override fun initView() {
         userLogoIv = findViewById(R.id.userLogoIv)
         userNameTv = findViewById(R.id.userNameTv)
         userInfoLayout = findViewById(R.id.userInfoLayout)
+        allOrderTv = findViewById(R.id.allOrderTv)
+        toPayTv = findViewById(R.id.toPayTv)
+        toDeliverTv = findViewById(R.id.toDeliverTv)
+        toReceiveTv = findViewById(R.id.toReceiveTv)
+        toCommentTv = findViewById(R.id.toCommentTv)
+        afterSalesTv = findViewById(R.id.afterSalesTv)
     }
 
     private fun refreshPage() {
         if (LoginManager.isLoggedIn()) {
             userLogoIv.setImageResource(R.drawable.ic_user_loggedin)
             userNameTv.text = "尊贵会员"
-            userInfoLayout.setOnClickListener { }
         } else {
             userLogoIv.setImageResource(R.drawable.ic_user_default)
             userNameTv.text = "登录/注册 >"
-            userInfoLayout.setOnClickListener {
-                startActivity(Intent(context, LoginActivity::class.java))
-            }
+        }
+
+        if (LoginManager.isLoggedIn()) {
+            userInfoLayout.setOnClickListener { }
+            allOrderTv.setOnClickListener { }
+            toPayTv.setOnClickListener {}
+            toDeliverTv.setOnClickListener { }
+            toReceiveTv.setOnClickListener { }
+            toCommentTv.setOnClickListener { }
+            afterSalesTv.setOnClickListener { }
+        } else {
+            userInfoLayout.setOnClickListener { gotoLoginPage() }
+            allOrderTv.setOnClickListener { gotoLoginPage() }
+            toPayTv.setOnClickListener { gotoLoginPage() }
+            toDeliverTv.setOnClickListener { gotoLoginPage() }
+            toReceiveTv.setOnClickListener { gotoLoginPage() }
+            toCommentTv.setOnClickListener { gotoLoginPage() }
+            afterSalesTv.setOnClickListener { gotoLoginPage() }
         }
     }
 
@@ -41,6 +66,10 @@ class MineFragment : BaseFragment() {
         findViewById<View>(R.id.settingsIv).setOnClickListener {
             startActivity(Intent(context, SettingsActivity::class.java))
         }
+    }
+
+    private fun gotoLoginPage() {
+        startActivity(Intent(context, LoginActivity::class.java))
     }
 
     override fun onResume() {
