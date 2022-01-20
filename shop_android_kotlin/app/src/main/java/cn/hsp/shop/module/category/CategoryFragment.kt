5d9bf65b7b9b4208d31bcb97a25e1f283ce1647e
jerry <309032663@qq.com>
2022-01-20 22:37:29 +0800

    update

--- shop_android_kotlin/app/src/main/java/cn/hsp/shop/module/category/CategoryFragment.kt
+++ shop_android_kotlin/app/src/main/java/cn/hsp/shop/module/category/CategoryFragment.kt
@@ -1,21 +1,30 @@
 package cn.hsp.shop.module.category
 
+import android.content.Intent
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import cn.hsp.shop.R
 import cn.hsp.shop.base.BaseVmFragment
+import cn.hsp.shop.module.search.SearchActivity
 import cn.hsp.shop.network.response.CategoryInfo
 
-class CategoryFragment() : BaseVmFragment<CategoryViewModel>() {
+class CategoryFragment : BaseVmFragment<CategoryViewModel>() {
+    private lateinit var homeSearchLayout: View
     private lateinit var viewPager: ViewPager
 
     override fun viewModelClass() = CategoryViewModel::class.java
     override fun layoutResId() = R.layout.fragment_category
 
     override fun initView() {
+        homeSearchLayout = findViewById(R.id.homeSearchLayout)
         viewPager = findViewById(R.id.viewPager)
+    }
+
+    override fun initListeners() {
+        homeSearchLayout.setOnClickListener { startActivity(Intent(context, SearchActivity::class.java)) }
     }
 
     override fun initData() {
