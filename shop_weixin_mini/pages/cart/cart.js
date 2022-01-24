46c2920e8c0030d59ee072ffdb00b71e6ed4cdd4
jerry <309032663@qq.com>
2022-01-24 21:57:34 +0800

    update

---
+++ shop_weixin_mini/pages/cart/cart.js
@@ -0,0 +1,48 @@
+//index.js
+//获取应用实例
+var http = require('../../utils/httputils.js');
+
+Page({
+  data: {
+    dataList: [],
+  },
+  onLoad: function () {
+    this.queryData()
+  },
+  onShow: function () {
+    this.queryData()
+  },
+  methods: {
+  },
+  queryData() {
+    let _this = this
+    http.get('shop/addr/query/', '',
+      function (resp) {
+        _this.setData({
+          dataList: resp.data
+        })
+      },
+      function (err) { })
+  },
+  gotoAddAddr() {
+    wx.navigateTo({
+      url: '/pages/addr/addr-add/addr-add'
+    })
+  },
+  gotoEditAddr(e) {
+    let userAddrId = e.currentTarget.dataset['id'];
+    wx.navigateTo({
+      url: '/pages/addr/addr-edit/addr-edit?id=' + userAddrId
+    })
+  },
+  deleteAddr(e) {
+    let userAddrId = e.currentTarget.dataset['id'];
+    let _this = this
+    http.get('shop/addr/del/' + userAddrId, '',
+      function (resp) {
+        _this.queryData()
+      },
+      function (err) { })
+  },
+
+})
