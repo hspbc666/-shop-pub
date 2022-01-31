603654205f8479519b4a78b02f985dfeea339bcb
jerry <309032663@qq.com>
2022-01-31 09:56:52 +0800

    update

--- shop_flutter/lib/pages/order/order_detail.dart
+++ shop_flutter/lib/pages/order/order_detail.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shop_flutter/constants.dart';
 import 'package:shop_flutter/network/bean/query_order_detail_resp.dart';
-import 'package:shop_flutter/network/http_manager.dart';
+import 'package:shop_flutter/pages/addr/addr_list.dart';
+import 'package:shop_flutter/pages/login/login_manager.dart';
 import 'package:shop_flutter/ui_kit.dart';
 
 /// 厦门大学计算机专业 | 前华为工程师
@@ -23,36 +25,92 @@ class _OrderDetailState extends State<OrderDetailPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: buildOrderBlock(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("订单详情"),
+        backgroundColor: LblColors.mainColor,
+      ),
+      body: Container(
+          margin: const EdgeInsets.fromLTRB(5, 20, 10, 0),
+          child: Column(
+            children: [
+              Expanded(
+                  child: Column(children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    //设置背景色
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    children: [
+                      buildUserInfo(),
+                      mySpacer(10),
+                      buildDivider(),
+                      mySpacer(10),
+                      buildRowWithArrow("收货地址", () {
+                        gotoAddrPage();
+                      })
+                    ],
+                  ),
+                  padding: const EdgeInsets.all(10),
+                ),
+                Container(
+                  decoration: const BoxDecoration(
+                    //设置背景色
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    children: [
+                      buildRowWithArrow("隐私设置", () {}),
+                      mySpacer(10),
+                      buildDivider(),
+                      mySpacer(10),
+                      buildRowWithArrow("APP版本", () {}),
+                      mySpacer(10),
+                      buildDivider(),
+                      mySpacer(10),
+                      buildRowWithArrow("隐私政策", () {}),
+                      mySpacer(10),
+                      buildDivider(),
+                      mySpacer(10),
+                      buildRowWithArrow("关于", () {})
+                    ],
+                  ),
+                  padding: const EdgeInsets.all(10),
+                  margin: EdgeInsets.only(top: 20),
+                )
+              ])),
+              buildBottomRow()
+            ],
+          )),
     );
   }
 
-  Column buildOrderBlock() {
-    return Column(
-      children: [
+  buildUserInfo() {
+    return Row(
+      children: <Widget>[
+        const Image(
+          image: AssetImage('assets/images/user.png'),
+          width: 50,
+          height: 50,
+        ),
+        Container(
+          padding: EdgeInsets.only(left: 20),
+          child: Column(
+            children: const [
+              Text("尊贵会员"),
+              Text("级别：白银", style: TextStyle(fontSize: 12.0, color: Color(0xFF9E9EA2))),
+            ],
+          ),
+        ),
+        Spacer(),
         Row(
-          children: const [Text("X商自营", style: TextStyle(fontSize: 18.0, color: Colors.black)), Spacer(), Text("状态")],
-        ),
-        Column(
-          children: buildOrderDetailList(),
-        ),
-        mySpacer(10),
-        const Divider(
-          height: 10,
-          thickness: 1,
-        ),
-        Row(
-          children: [
-            Spacer(),
-            OutlinedButton(
-              child: const Text('发票详情', style: TextStyle(color: Color(0xFF575E64))),
-              onPressed: () {},
-            ),
-            myVerticalSpacer(10),
-            OutlinedButton(
-              child: const Text('申请售后', style: TextStyle(color: Color(0xFF575E64))),
-              onPressed: () {},
+          children: const [
+            Text('查看个人资料'),
+            Image(
+              image: AssetImage('assets/images/right_arrow.png'),
+              width: 20,
+              height: 20,
             )
           ],
         )
@@ -60,49 +118,41 @@ class _OrderDetailState extends State<OrderDetailPage> {
     );
   }
 
-  List<Column> buildOrderDetailList() {
-    if (_queryOrderDetailData == null) {
-      return emptyContainer();
-    } else {
-      return _queryOrderDetailData!.list.map((QueryOrderDetailDataItem queryOrderDetailDataItem) {
-        return Column(
-          children: [
-            Row(
-              children: [
-                Image(
-                  image: NetworkImage(queryOrderDetailDataItem.squarePic),
-                  width: 100,
-                  height: 100,
-                ),
-                myVerticalSpacer(10),
-                Expanded(child: Text(queryOrderDetailDataItem.name, maxLines: 2, overflow: TextOverflow.ellipsis)),
-                Column(
-                  children: [
-                    Text("￥" + (queryOrderDetailDataItem.price / 100).toString()),
-                    Text("×" + queryOrderDetailDataItem.quantity.toString())
-                  ],
-                )
-              ],
-            ),
-          ],
-        );
-      }).toList();
-    }
+  buildRowWithArrow(String text, GestureTapCallback? onTap) {
+    return InkWell(
+      child: Row(
+        children: <Widget>[
+          Text(text),
+          const Spacer(),
+          const Image(
+            image: AssetImage('assets/images/right_arrow.png'),
+            width: 20,
+            height: 20,
+          )
+        ],
+      ),
+      onTap: onTap,
+    );
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _queryData();
+  Container buildBottomRow() {
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      width: double.infinity,
+      child: SizedBox(
+        child: ElevatedButton(
+          child: const Text('退出登录', style: const TextStyle(color: Color(0xFFEF3965))),
+          onPressed: () {
+            LoginManager.logout();
+            Navigator.pop(context);
+          },
+          style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.white)),
+        ),
+      ),
+    );
   }
 
-  _queryData() async {
-    String url = "shop/order/query/" + widget.orderId;
-    HttpManager.getInstance().get(url).then((resp) {
-      var result = QueryOrderDetailResp.fromJson(resp);
-      setState(() {
-        _queryOrderDetailData = result.data;
-      });
-    });
+  void gotoAddrPage() {
+    Navigator.push(context, MaterialPageRoute(builder: (context) => AddrListPage()));
   }
 }
