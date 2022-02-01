45f23c832019fd451f7c601c93b6673154a66891
jerry <309032663@qq.com>
2022-02-01 16:35:12 +0800

    update

--- shop_flutter/lib/pages/home.dart
+++ shop_flutter/lib/pages/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper_null_safety/flutter_swiper_null_safety.dart';
+import 'package:shop_flutter/lblbc_ui_kit.dart';
 import 'package:shop_flutter/network/bean/query_goods_by_category_resp_entity.dart';
 import 'package:shop_flutter/network/http_manager.dart';
 import 'package:shop_flutter/pages/goods.dart';
@@ -18,6 +20,11 @@ class HomePage extends StatefulWidget {
 
 class _HomeState extends State<HomePage> {
   List<QueryGoodsByCategoryRespData> _goodsList = [];
+  List<String> imageUrls = [
+    "https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2022/01/06/102/ias_a28a4efde3be0890bb22724e5dedaeb5_1135x545_85.jpg",
+    "https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2021/07/30/160/ias_86095df3cfe17ce6437098d7d0519d9f_1135x545_85.jpg",
+    "https://h2.appsimg.com/a.appsimg.com/upload/brand/upcb/2021/11/01/161/ias_f2b8d6ecbc6ec61a89d20d19a6a98b8c_1135x545_85.jpg",
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -29,8 +36,29 @@ class _HomeState extends State<HomePage> {
               gotoSearchPage();
             })
       ]),
-      body: Text("dddd"),
+      body: Column(
+        children: [_buildBanner()],
+      ),
     );
+  }
+
+  _buildBanner() {
+    if (_goodsList.isEmpty) {
+      return emptyContainer();
+    } else {
+      return Container(
+          height: 200,
+          child: Swiper(
+            itemCount: 3,
+            itemBuilder: (BuildContext context, int index) {
+              return Image.network(
+                _goodsList[index].longPic,
+                fit: BoxFit.cover,
+              );
+            },
+            pagination: const SwiperPagination(),
+          ));
+    }
   }
 
   onRowClick(QueryGoodsByCategoryRespData queryGoodsByCategoryRespData) {
