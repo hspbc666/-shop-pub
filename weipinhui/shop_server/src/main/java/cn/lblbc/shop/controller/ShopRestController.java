3585d0bc54c2014f52da67ab87a708563d2d9d7e
jerry <309032663@qq.com>
2022-03-25 21:46:35 +0800

    update

--- weipinhui/shop_server/src/main/java/cn/lblbc/shop/controller/ShopRestController.java
+++ weipinhui/shop_server/src/main/java/cn/lblbc/shop/controller/ShopRestController.java
@@ -48,42 +48,35 @@ public class ShopRestController {
     @Autowired
     private JwtUtils jwtUtils;
 
-    @GetMapping("goods/list")
-    public Resp<List<Goods>> queryAllGoods() {
-        Resp<List<Goods>> resp = new Resp<>();
-        resp.setData(goodsService.queryAll());
-        return resp;
-    }
-
-    @GetMapping("category")
+    @GetMapping("categories")
     public Resp<List<CategoryInfo>> queryCategory() {
         Resp<List<CategoryInfo>> resp = new Resp<>();
         resp.setData(categoryService.query());
         return resp;
     }
 
-    @GetMapping("goods/category/{categoryId}")
-    public Resp<List<Goods>> queryGoodsByCategory(@PathVariable String categoryId) {
+    @GetMapping("goods")
+    public Resp<List<Goods>> queryGoodsByCategory(@RequestParam("categoryId") String categoryId) {
         Resp<List<Goods>> resp = new Resp<>();
         resp.setData(goodsService.queryByCategory(categoryId));
         return resp;
     }
 
-    @GetMapping("goods/query/{goodsId}")
+    @GetMapping("goodsBySearch")
+    public Resp<List<Goods>> searchGoods(@RequestParam("keyword") String keyword) {
+        Resp<List<Goods>> resp = new Resp<>();
+        resp.setData(goodsService.queryByName(keyword));
+        return resp;
+    }
+
+    @GetMapping("goods/{goodsId}")
     public Resp<Goods> queryGoods(@PathVariable String goodsId) {
         Resp<Goods> resp = new Resp<>();
         resp.setData(goodsService.queryById(goodsId));
         return resp;
     }
 
-    @GetMapping("goods/search/{keyword}")
-    public Resp<List<Goods>> searchGoods(@PathVariable String keyword) {
-        Resp<List<Goods>> resp = new Resp<>();
-        resp.setData(goodsService.queryByName(keyword));
-        return resp;
-    }
-
-    @GetMapping("cart/list")
+    @GetMapping("cart")
     public Resp<List<CartItem>> queryCart(@RequestHeader("Authorization") String authorization) {
         int userId = getUserIdFromHeader(authorization);
         Resp<List<CartItem>> resp = new Resp<>();
@@ -91,36 +84,35 @@ public class ShopRestController {
         return resp;
     }
 
-    @GetMapping("cart/queryByIds/{cartIds}")
+    @GetMapping("cart/{cartIds}")
     public Resp<List<CartItem>> queryCartByIds(@PathVariable String cartIds) {
         Resp<List<CartItem>> resp = new Resp<>();
         resp.setData(cartService.queryByCartIds(cartIds));
         return resp;
     }
 
-    @GetMapping("cart/add/{goodsId}")
+    @PostMapping("cart/{goodsId}")
     public Resp<Goods> addToCart(@PathVariable String goodsId, @RequestHeader("Authorization") String authorization) {
         int userId = getUserIdFromHeader(authorization);
         cartService.addToCart(userId, goodsId, 1);
         return new Resp<>();
     }
 
-    @GetMapping("cart/modify/{cartId}/{quantity}")
+    @PutMapping("cart/{cartId}/{quantity}")
     public Resp<Goods> modifyCart(@PathVariable String cartId, @PathVariable int quantity, @RequestHeader("Authorization") String authorization) {
         cartService.modifyCart(cartId, quantity);
         return new Resp<>();
     }
 
-    @PostMapping("order/createFromCart")
-    public Resp<CreateOrderResp> createFromCart(@RequestBody CreateOrderFromCartReq req, @RequestHeader("Authorization") String authorization) {
+    @GetMapping("orders")
+    public Resp<List<QueryOrderResp>> queryByStatus(@RequestParam("orderStatus") int orderStatus, @RequestHeader("Authorization") String authorization) {
         int userId = getUserIdFromHeader(authorization);
-        String orderId = orderService.createOrderFromCart(userId, req.getCartIdList(), req.getAddressId());
-        Resp<CreateOrderResp> resp = new Resp<>();
-        resp.setData(CreateOrderResp.builder().orderId(orderId).build());
+        Resp<List<QueryOrderResp>> resp = new Resp<>();
+        resp.setData(orderService.queryOrder(userId, orderStatus));
         return resp;
     }
 
-    @PostMapping("order/create")
+    @PostMapping("orders")
     public Resp<CreateOrderResp> createOrder(@RequestBody CreateOrderReq req, @RequestHeader("Authorization") String authorization) {
         int userId = getUserIdFromHeader(authorization);
         String orderId = orderService.createOrder(userId, req.getGoodsId(), req.getAddressId());
@@ -129,49 +121,44 @@ public class ShopRestController {
         return resp;
     }
 
-    @GetMapping("order/pay/{orderId}")
-    public Resp<CreateOrderResp> payForOrder(@PathVariable String orderId) {
-        orderService.changeOrderStatus(orderId, TO_DELIVER);
-        return new Resp<>();
-    }
-
-    @GetMapping("order/queryByStatus/{orderStatus}")
-    public Resp<List<QueryOrderResp>> queryByStatus(@PathVariable int orderStatus, @RequestHeader("Authorization") String authorization) {
+    @PostMapping("ordersFromCart")
+    public Resp<CreateOrderResp> createFromCart(@RequestBody CreateOrderFromCartReq req, @RequestHeader("Authorization") String authorization) {
         int userId = getUserIdFromHeader(authorization);
-        Resp<List<QueryOrderResp>> resp = new Resp<>();
-        resp.setData(orderService.queryOrder(userId, orderStatus));
+        String orderId = orderService.createOrderFromCart(userId, req.getCartIdList(), req.getAddressId());
+        Resp<CreateOrderResp> resp = new Resp<>();
+        resp.setData(CreateOrderResp.builder().orderId(orderId).build());
         return resp;
     }
 
-    @GetMapping("order/query/{orderId}")
+    @GetMapping("orders/{orderId}")
     public Resp<QueryOrderResp> queryOrder(@PathVariable String orderId) {
         Resp<QueryOrderResp> resp = new Resp<>();
         resp.setData(orderService.queryOrder(orderId));
         return resp;
     }
 
-    @GetMapping("order/del/{orderId}")
+    @DeleteMapping("orders/{orderId}")
     public Resp<String> deleteOrder(@PathVariable String orderId) {
         orderService.delete(orderId);
         return new Resp<>();
     }
 
-    @GetMapping("addr/query")
-    public Resp<List<Address>> queryAddr(@RequestHeader("Authorization") String authorization) {
+    @GetMapping("addrs")
+    public Resp<List<Address>> queryAddress(@RequestHeader("Authorization") String authorization) {
         int userId = getUserIdFromHeader(authorization);
         Resp<List<Address>> resp = new Resp<>();
         resp.setData(addressService.query(userId));
         return resp;
     }
 
-    @GetMapping("addr/query/{addressId}")
-    public Resp<Address> queryAddrById(@PathVariable String addressId) {
+    @GetMapping("addrs/{addressId}")
+    public Resp<Address> queryAddressById(@PathVariable String addressId) {
         Resp<Address> resp = new Resp<>();
         resp.setData(addressService.queryById(addressId));
         return resp;
     }
 
-    @GetMapping("addr/query_default")
+    @GetMapping("addr_default")
     public Resp<Address> queryDefaultAddress(@RequestHeader("Authorization") String authorization) {
         int userId = getUserIdFromHeader(authorization);
         Resp<Address> resp = new Resp<>();
@@ -179,8 +166,8 @@ public class ShopRestController {
         return resp;
     }
 
-    @PostMapping("addr/add")
-    public Resp<String> addAddr(@RequestBody Address address, @RequestHeader("Authorization") String authorization) {
+    @PostMapping("addrs")
+    public Resp<String> addAddress(@RequestBody Address address, @RequestHeader("Authorization") String authorization) {
         int userId = getUserIdFromHeader(authorization);
         String addressId = LblIdGenerator.generateId();
         address.setId(addressId);
@@ -191,15 +178,15 @@ public class ShopRestController {
         return new Resp<>();
     }
 
-    @PostMapping("addr/modify")
-    public Resp<List<Address>> modifyAddr(@RequestBody Address address, @RequestHeader("Authorization") String authorization) {
+    @PutMapping("addrs")
+    public Resp<List<Address>> modifyAddress(@RequestBody Address address, @RequestHeader("Authorization") String authorization) {
         int userId = getUserIdFromHeader(authorization);
         addressService.modify(userId, address);
         return new Resp<>();
     }
 
-    @GetMapping("addr/del/{addressId}")
-    public Resp<String> deleteAddr(@PathVariable String addressId) {
+    @DeleteMapping("addrs/{addressId}")
+    public Resp<String> deleteAddress(@PathVariable String addressId) {
         addressService.delete(addressId);
         return new Resp<>();
     }
