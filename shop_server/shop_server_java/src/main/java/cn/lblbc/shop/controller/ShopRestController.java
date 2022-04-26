6a1bb7dfb281e8caec1ecb83364ae3337f7d550b
jerry <309032663@qq.com>
2022-04-26 22:59:31 +0800

    update

--- shop_server/shop_server_java/src/main/java/cn/lblbc/shop/controller/ShopRestController.java
+++ shop_server/shop_server_java/src/main/java/cn/lblbc/shop/controller/ShopRestController.java
@@ -21,7 +21,7 @@ import static cn.lblbc.utils.Constants.OrderStatus.TO_DELIVER;
 
 /**
  * 厦门大学计算机专业 | 前华为工程师
- * 专注《零基础学编程系列》  https://lblbc.cn/blog
+ * 专注《零基础学编程系列》  http://lblbc.cn/blog
  * 包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
  * 公众号：蓝不蓝编程
  */
