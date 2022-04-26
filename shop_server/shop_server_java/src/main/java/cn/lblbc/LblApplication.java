750bfed7b00b61dd6a22ba7368b229d0d604bc76
jerry <309032663@qq.com>
2022-04-26 22:34:06 +0800

    update

--- shop_server/shop_server_java/src/main/java/cn/lblbc/LblApplication.java
+++ shop_server/shop_server_java/src/main/java/cn/lblbc/LblApplication.java
@@ -1,35 +1,18 @@
 package cn.lblbc;
 
-import org.apache.catalina.connector.Connector;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 /**
  * 厦门大学计算机专业 | 前华为工程师
- * 专注《零基础学编程系列》https://cxyxy.blog.csdn.net/article/details/121134634
+ * 专注《零基础学编程系列》  https://lblbc.cn/blog
  * 包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
  * 公众号：蓝不蓝编程
  */
 @MapperScan({"cn.lblbc.login.mapper", "cn.lblbc.shop.mapper"}) //扫描的mapper
 @SpringBootApplication
 public class LblApplication {
-    @Value("${http.port}")
-    private Integer port;
-
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setPort(port);
-        tomcat.addAdditionalTomcatConnectors(connector);
-        return tomcat;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(LblApplication.class, args);
     }
