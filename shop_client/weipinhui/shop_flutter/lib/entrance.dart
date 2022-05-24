6812a42ad67e1c7c1bed8140a6d14bf17e773b44
蓝不蓝编程 <309032663@qq.com>
2022-05-24 00:52:59 +0000

    update shop_client/weipinhui/shop_flutter/lib/entrance.dart.

--- shop_client/weipinhui/shop_flutter/lib/entrance.dart
+++ shop_client/weipinhui/shop_flutter/lib/entrance.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop_flutter/lblbc_constants.dart';
 
-import 'pages/home/home.dart';
+import 'pages/main.dart';
 
 /// 厦门大学计算机专业 | 前华为工程师
 /// 专注《零基础学编程系列》https://cxyxy.blog.csdn.net/article/details/121134634
