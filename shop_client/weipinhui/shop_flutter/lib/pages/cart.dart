0e6f3bc6c88e531db70813caea55d81149d9cd4f
jerry <309032663@qq.com>
2022-06-07 22:37:17 +0800

    update

--- shop_client/weipinhui/shop_flutter/lib/pages/cart.dart
+++ shop_client/weipinhui/shop_flutter/lib/pages/cart.dart
@@ -11,7 +11,7 @@ import 'package:shop_flutter/pages/login/login_manager.dart';
 import 'order/order_confirm.dart';
 
 /// 厦门大学计算机专业 | 前华为工程师
-/// 专注《零基础学编程系列》https://cxyxy.blog.csdn.net/article/details/121134634
+/// 专注《零基础学编程系列》  http://lblbc.cn/blog
 /// 包含：Java | 安卓 | 前端 | Flutter | iOS | 小程序 | 鸿蒙
 /// 公众号：蓝不蓝编程
 
