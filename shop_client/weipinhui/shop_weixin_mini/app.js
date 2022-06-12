6031be2d53697517051c789ad6008e071b03e7f2
jerry <309032663@qq.com>
2022-06-12 22:16:21 +0800

    update

--- shop_client/weipinhui/shop_weixin_mini/app.js
+++ shop_client/weipinhui/shop_weixin_mini/app.js
@@ -8,12 +8,10 @@ App({
   userInfo: "",
 
   setGlobalUserInfo: function (user) {
-    //设置用户对象为缓存的方法(key,data)
     wx.setStorageSync("userInfo", user);
   },
 
   getGlobalUserInfo: function () {
-    //获取缓存的用户对象
     return wx.getStorageSync("userInfo");
   },
 
