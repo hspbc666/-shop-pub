2d85616c6e651c159a754a9bdc52e496b8baa0b9
jerry <309032663@qq.com>
2022-09-17 22:19:24 +0800

    update

--- shop_client/weipinhui/shop_android_kotlin_mvvm/app/src/main/java/cn/lblbc/shop/module/search/SearchActivity.kt
+++ shop_client/weipinhui/shop_android_kotlin_mvvm/app/src/main/java/cn/lblbc/shop/module/search/SearchActivity.kt
@@ -2,14 +2,15 @@ package cn.lblbc.shop.module.search
 
 import android.content.Intent
 import android.view.inputmethod.EditorInfo
-import cn.lblbc.lib.utils.hideSoftKeyboard
-import cn.lblbc.lib.utils.showSoftKeyboard
+import cn.lblbc.lib.utils.*
+import cn.lblbc.lib.view.LblRecyclerView
 import cn.lblbc.shop.R
 import cn.lblbc.shop.base.BaseVmActivity
 import cn.lblbc.shop.module.goods_detail.GoodsActivity
 import cn.lblbc.shop.module.goods_detail.GoodsAdapter
 import cn.lblbc.shop.network.Goods
 import cn.lblbc.shop.utils.EXTRA_KEY_GOODS_ID
+import kotlinx.android.synthetic.main.item_goods.view.*
 import kotlinx.android.synthetic.main.part_search_top.*
 
 /**
@@ -19,54 +20,36 @@ import kotlinx.android.synthetic.main.part_search_top.*
  * 公众号：蓝不蓝编程
  */
 class SearchActivity : BaseVmActivity<SearchViewModel>() {
-    private lateinit var goodsAdapter: GoodsAdapter
+    private lateinit var lblRecyclerView: LblRecyclerView<Goods>
     override fun viewModelClass() = SearchViewModel::class.java
     override fun layoutResId(): Int = R.layout.activity_search
     override fun initView() {
-        goodsAdapter = GoodsAdapter(this)
-//        goodsGridView.adapter = goodsAdapter
-//        goodsGridView.setOnItemClickListener { _, _, position, _ -> onItemClick(goodsAdapter.getData(position)) }
+        lblRecyclerView = findViewById(R.id.lblRecyclerView)
+        lblRecyclerView.setLayoutResId { R.layout.item_goods }
+        lblRecyclerView.setColumns(2)
+        lblRecyclerView.setOnBind { itemView, data ->
+            itemView.goodsNameTv.text = data.name
+            itemView.goodsPriceTv.text = getMoneyByYuan(data.price)
+            loadImage(this, itemView.goodsIv, data.squarePic)
+        }
+        lblRecyclerView.setOnItemClick { onItemClick(it) }
         showSoftKeyboard(this, searchEt)
     }
 
     override fun initListeners() {
         backIv.setOnClickListener { finish() }
-//        searchEt.setOnClickListener {
-//            noDataLayout.visibility = GONE
-//            goodsGridView.visibility = GONE
-//            searchHisView.visibility = VISIBLE
-//        }
-        searchEt.setOnEditorActionListener { _, keyCode, _ ->
-            if (keyCode == EditorInfo.IME_ACTION_SEARCH) {
-                search()
-            }
-            true
-        }
-        searchTv.setOnClickListener { search() }
-//        searchHisView.setCallback {
-//            searchEt.setText(it)
-//            search()
-//        }
+        searchEt.onSearchKeyDown { search() }
     }
 
     private fun search() {
         val keyword = searchEt.text.toString()
         mViewModel.queryGoods(keyword)
-//        searchHisView.addKeyword(keyword)
         hideSoftKeyboard(this)
     }
 
     override fun observe() {
         mViewModel.goodsList.observe(this) {
-//            if (it.isEmpty()) {
-//                noDataLayout.visibility = VISIBLE
-//                goodsGridView.visibility = GONE
-//                searchHisView.visibility = GONE
-//            } else {
-//                noDataLayout.visibility = GONE
-//                goodsGridView.visibility = VISIBLE
-//                goodsAdapter.setData(it)
-//            }
+            lblRecyclerView.setData(it)
         }
     }
 
